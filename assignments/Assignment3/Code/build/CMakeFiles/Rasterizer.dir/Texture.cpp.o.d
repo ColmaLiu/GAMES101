082ab311f326aa4831dd7322e85dec052cbb89eb
Colma <2693752619@qq.com>
2024-10-08 21:12:52 +0800

    first commit

---
+++ assignments/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/Texture.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Rasterizer.dir/Texture.cpp.o: \
+ /home/colma/GAMES101/Assignment3/Code/Texture.cpp \
+ /usr/include/stdc-predef.h
---
+++ assignments/assignment8/build/CGL/src/CMakeFiles/CGL.dir/osdfont.c.o.d
@@ -0,0 +1,3 @@
+CGL/src/CMakeFiles/CGL.dir/osdfont.c.o: \
+ /home/colma/GAMES101/assignment8/CGL/src/osdfont.c \
+ /usr/include/stdc-predef.h
